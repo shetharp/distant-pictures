7f5ec663dc4b6ff49ee3732442eac7a49eaab809
Arpit Sheth <shetharp@gmail.com>
2018-02-13 03:23:16 -0500

    strips styles

--- public/client.js
+++ public/client.js
@@ -50,11 +50,11 @@ socket.on('server-msg', function(msg) {
   console.log('msg:', msg);
   switch (msg) {
     case "light":
-      document.body.style.backgroundColor = "white";
+      // document.body.style.backgroundColor = "white";
       console.log("white")
       break;
     case "dark":
-      document.body.style.backgroundColor = "black";
+      // document.body.style.backgroundColor = "black";
       console.log("black");
       break;
     default:
