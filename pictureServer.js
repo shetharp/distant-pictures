a6f10fd0c982a592b8e0d473ce537425dc01fd02
Arpit Sheth <shetharp@gmail.com>
2018-02-13 02:41:56 -0500

    stringifies the color pallete

--- pictureServer.js
+++ pictureServer.js
@@ -96,11 +96,11 @@ function takePicture() {
                {apply: 'saturate', params: [20]}
              ])
              .write("public/" + imageName + "-palette.jpg"); // save 
-        palette.push(JSON.stringify(image.intToRGBA( image.getPixelColor(150,150) )));
-        palette.push(JSON.stringify(image.intToRGBA( image.getPixelColor(50,50)   )));
-        palette.push(JSON.stringify(image.intToRGBA( image.getPixelColor(250,50)  )));
-        palette.push(JSON.stringify(image.intToRGBA( image.getPixelColor(50,250)  )));
-        palette.push(JSON.stringify(image.intToRGBA( image.getPixelColor(250,250) )));
+        palette.push(JSON.stringify(Jimp.intToRGBA( image.getPixelColor(150,150) )));
+        palette.push(JSON.stringify(Jimp.intToRGBA( image.getPixelColor(50,50)   )));
+        palette.push(JSON.stringify(Jimp.intToRGBA( image.getPixelColor(250,50)  )));
+        palette.push(JSON.stringify(Jimp.intToRGBA( image.getPixelColor(50,250)  )));
+        palette.push(JSON.stringify(Jimp.intToRGBA( image.getPixelColor(250,250) )));
         paletteData["colors"] = palette.toString();
         io.emit('newPalette', paletteData)
     }).catch(function (err) {
